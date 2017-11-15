b44267c4c3698d6daafc4d65fa82136b361b8d9f
Matt Henkel <guildencrantz@menagerie.cc>
2017-11-14 21:49:56 -0700

    Switch to PIA TCP Strong configs.

--- openvpn.sh
+++ openvpn.sh
@@ -6,10 +6,10 @@ if [ -n "$REGION" ]; then
   set -- "$@" '--config' "${REGION}.ovpn"
 fi
 
-if [ -n "$USERNAME" -a -n "$PASSWORD" ]; then
-  echo "$USERNAME" > auth.conf
-  echo "$PASSWORD" >> auth.conf
-  set -- "$@" '--auth-user-pass' 'auth.conf'
-fi
+#if [ -n "$USERNAME" -a -n "$PASSWORD" ]; then
+#  echo "$USERNAME" > auth.conf
+#  echo "$PASSWORD" >> auth.conf
+#  set -- "$@" '--auth-user-pass' 'auth.conf'
+#fi
 
 openvpn "$@"
